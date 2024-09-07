efc9ea5944ec4d6db36f6634e36385d65f1da64a
samlimby <129619263+samlimby@users.noreply.github.com>
2024-09-07 19:35:22 +0100

    generating multiple  astroids

--- script.js
+++ script.js
@@ -5,56 +5,69 @@ gameCanvas.height = 560;
 const c = gameCanvas.getContext("2d");
 
 let levelSpeed = 5;
-const astroidArray = Math.floor(Math.random() * levelSpeed);
 
 window.addEventListener("click", function(){
     playerShape();
-    astroidShape(astroidArray);
+    for (let i = 0; i < 5; i++) {
+        astroidShape();
+    };
+
+    function playerShape() {
+        let x = 200;
+        let y = 400;
+        let dx = 25;
+        let dy = 25;
+        function draw() {
+            c.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+            c.fillStyle = "black";
+            c.fillRect(x, y, 56, 56);
+        };
+    
+        // Event listener for keydown event
+        window.addEventListener("keydown", function(event){
+            if (event.key === "w" || event.key === "W") { 
+                y -= dy; 
+                draw();
+            } else if (event.key === "s" || event.key === "S") {
+                y += dy; 
+                draw();
+            }
+    
+            if (event.key === "a" || event.key === "A") {
+                x -= dx;
+                draw();
+            } else if (event.key === "d" || event.key === "D") {
+                x += dx;
+                draw();
+            }
+        });
+    
+        draw();
+    };
+
+    function astroidShape() {
+        const astroidArray = [];
+        const astroidItem = Math.floor(Math.random() * levelSpeed);
+    
+        let x = gameCanvas.height / 2;
+        let y = Math.floor(Math.random() * gameCanvas.height);
+        let dx = levelSpeed;
+        let dy = levelSpeed;
+    
+        function animate() {
+        };
+        
+        function draw() {
+            c.fillStyle = "gray";
+            c.fillRect(x, y, 32, 32);
+            astroidArray.push();
+            console.log(astroidArray);
+                
+        };
+
+        draw();
+    };
 
 });
 
-function playerShape() {
-    let x = 200;
-    let y = 400;
-    let dx = 25;
-    let dy = 25;
-    function draw() {
-        c.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-        c.fillStyle = "black";
-        c.fillRect(x, y, 56, 56);
-    };
-
-    // Event listener for keydown event
-    window.addEventListener("keydown", function(event){
-        if (event.key === "w" || event.key === "W") { 
-            y -= dy; 
-            draw();
-        } else if (event.key === "s" || event.key === "S") {
-            y += dy; 
-            draw();
-        }
-
-        if (event.key === "a" || event.key === "A") {
-            x -= dx;
-            draw();
-        } else if (event.key === "d" || event.key === "D") {
-            x += dx;
-            draw();
-        }
-    });
-
-    draw();
-};
-
-function astroidShape(productionRate) {
-    let x = gameCanvas.height / 2;
-    let y = Math.floor(Math.random() * gameCanvas.height);
-
-    function draw() {
-        c.fillStyle = "gray";
-        c.fillRect(x, y, 32, 32);
-    }
-
-    draw();
-};
 
