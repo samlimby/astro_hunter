625ec5e73d1e70cbe24170768219d1ea85c919fe
samlimby <129619263+samlimby@users.noreply.github.com>
2024-09-07 22:54:14 +0100

    update to movement functionality

--- script.js
+++ script.js
@@ -5,69 +5,80 @@ gameCanvas.height = 560;
 const c = gameCanvas.getContext("2d");
 
 let levelSpeed = 5;
+let astroidCount = 0;
+
+const playerXMovement = 2;
+const playerYMovement = 2;
+
+// functions to draw both shapes initially
+// functions to update and apply movement to both shapes
+
+let playerShape = {
+    x: 200, 
+    y: 400,
+    dx: playerXMovement,
+    dy: playerYMovement,
+    radius: 56
+};
+
+let astroidShape = {
+    x: Math.floor(Math.random() * gameCanvas.width),
+    y: Math.floor(Math.random() * gameCanvas.height),
+    dx: levelSpeed,
+    dy: levelSpeed,
+    radius: 48
+};
+
+const astroidArray = [];
+
 
 window.addEventListener("click", function(){
-    playerShape();
-    for (let i = 0; i < 5; i++) {
-        astroidShape();
+
+    function drawPlayer() {
+        c.fillStyle = "black";
+        c.fillRect(playerShape.x, playerShape.y, playerShape.radius, playerShape.radius);
+    };
+    
+    function drawAstroid() {
+        c.fillStyle = "red";
+        c.fillRect(astroidShape.x, astroidShape.y, astroidShape.radius, astroidShape.radius);
+    };
+    
+    function playerUpdate() {
+        playerShape.x += playerShape.dx;
+        playerShape.y += playerShape.dy;
+    }
+    
+    function astroidUpdate() {
+        astroidShape.x += astroidShape.dx;
+        astroidShape.y += astroidShape.dy;
+    }
+    
+    function animate() {
+        requestAnimationFrame(animate) 
+        c.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+        drawPlayer();
+        drawAstroid();
+        playerUpdate();
+        astroidUpdate();
     };
 
-    function playerShape() {
-        let x = 200;
-        let y = 400;
-        let dx = 25;
-        let dy = 25;
-        function draw() {
-            c.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-            c.fillStyle = "black";
-            c.fillRect(x, y, 56, 56);
-        };
-    
-        // Event listener for keydown event
-        window.addEventListener("keydown", function(event){
-            if (event.key === "w" || event.key === "W") { 
-                y -= dy; 
-                draw();
-            } else if (event.key === "s" || event.key === "S") {
-                y += dy; 
-                draw();
-            }
-    
-            if (event.key === "a" || event.key === "A") {
-                x -= dx;
-                draw();
-            } else if (event.key === "d" || event.key === "D") {
-                x += dx;
-                draw();
-            }
-        });
-    
-        draw();
-    };
+    window.addEventListener("keydown", function(event){
+        if (event.key === "w" || event.key === "W" || event.key === "ArrowUp") {
+            playerShape.y = -playerYMovement
+            console.log(playerShape.y)
+        } else if (event.key === "s" || event.key === "S" || event.key === "ArrowDown") {
+            playerShape.y = playerYMovement
+        } else if (event.key === "a" || event.key === "A" || event.key === "ArrowLeft") {
+            playerShape.x = -playerXMovement
+        } else if (event.key === "d" || event.key === "D" || event.key === "ArrowRight") {
+            playerShape.x = playerXMovement
+        }
+    });
 
-    function astroidShape() {
-        const astroidArray = [];
-        const astroidItem = Math.floor(Math.random() * levelSpeed);
-    
-        let x = gameCanvas.height / 2;
-        let y = Math.floor(Math.random() * gameCanvas.height);
-        let dx = levelSpeed;
-        let dy = levelSpeed;
-    
-        function animate() {
-        };
-        
-        function draw() {
-            c.fillStyle = "gray";
-            c.fillRect(x, y, 32, 32);
-            astroidArray.push();
-            console.log(astroidArray);
-                
-        };
-
-        draw();
-    };
+    animate();
 
 });
 
-
+console.log(playerShape.x, playerShape.y)
+console.log(astroidShape.x, astroidShape.y)
